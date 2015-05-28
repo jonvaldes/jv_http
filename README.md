6122d7e5fdaedddb5e80a3a67df17461e0968a17
Jon Valdes <juanval@gmail.com>
2015-05-28 22:05:03 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # jv_http
 Single-header library for simple HTTP requests
+
+This library does simple HTTP requests, with custom headers if desired. It's 
+designed to be as easy to integrate as possible. Look at the file itself to 
+see some usage explanations and a test case.
+
+Limitations:
+ - Right now only the winsock version is implemented, but a BSD socket 
+version is planned too.
+ - Chunked and gzipped transport are not implemented, and neither is HTTPS. 
+ Thus, this library is limited to work in the HTTP 1.0 subset, and it still 
+ doesn't implement the whole standard. Only just enough to get by.
+
+To build the test case, do:
+
+    cl.exe -D JV_HTTP_TEST -TC "jv_http.h" -link
+
+This will produce a test program that downloads a url and prints it to stdout.
+
+All comments and improvements are welcome.
---
+++ jv_http.h
@@ -0,0 +1,403 @@
+/* jv_http - v0.1 public domain http library http://github.com/jonvaldes/jv_http
+                               no warranty implied; use it at your own risk
+
+   Do this:
+     #define JV_HTTP_IMPLEMENTATION
+   before you include this file in *one* C or C++ file to create the implementation.
+
+   i.e. it should look like this:
+   	 #include ...
+     #include ...
+     #include ...
+     #define JV_HTTP_IMPLEMENTATION
+     #include "jv_http.h"
+ 
+License:
+   This software is in the public domain. Where that dedication is not
+   recognized, you are granted a perpetual, irrevocable license to copy
+   and modify this file however you want.
+
+Acknowledgements:
+   This library was inspired by Sean T. Barrett's excellent "stb" libraries, available at http://nothings.org
+*/
+
+#ifndef JV_INCLUDE_HTTP_H
+#define JV_INCLUDE_HTTP_H
+
+#ifdef JV_HTTP_STATIC
+#define JVHDEF static
+#else
+#define JVHDEF extern
+#endif
+
+#ifndef JV_HTTP_MALLOC
+#define JV_HTTP_MALLOC malloc
+#endif
+
+#ifndef JV_HTTP_FREE
+#define JV_HTTP_FREE free
+#endif
+
+#include "stdio.h"
+
+#ifdef _MSC_VER
+#define JV_PLATFORM_WINSOCK
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32")
+#pragma comment(lib, "Mswsock")
+#else
+#define JV_PLATFORM_BSDSOCK
+#endif
+
+struct jvh_env;
+typedef enum {
+    JVH_ERR_OK = 0,
+    JVH_ERR_UNSUPPORTED,
+    JVH_ERR_OUT_OF_MEMORY,
+    JVH_ERR_PERMISSION_DENIED,
+
+    JVH_ERR_INVALID_RESPONSE,
+
+    JVH_ERR_CONN_REFUSED,
+    JVH_ERR_CONN_ABORTED,
+    JVH_ERR_CONN_RESET,
+
+    JVH_ERR_HOST_UNREACHABLE,
+    JVH_ERR_NET_UNREACHABLE,
+    JVH_ERR_NET_DOWN,
+    JVH_ERR_TIMEOUT,
+
+    JVH_ERR_BAD_MEM_ADDRESS,
+    JVH_ERR_INVALID_ARGUMENT,
+
+    JVH_ERR_TOO_MANY_CONNS,
+
+    JVH_ERR_UNKNOWN = 5000,
+} jvh_error;
+
+#define JV_HTTP_RESPONSE_BUFFER_LEN 8192
+
+typedef struct {
+    int status_code;
+    char _buffer[JV_HTTP_RESPONSE_BUFFER_LEN];
+    int _bytes_in_buffer;
+    int _buffer_offset;
+    char _internal[4]; // Enough bytes to hold internal platform-dependent data
+} jvh_response;
+
+JVHDEF jvh_error jvh_init(struct jvh_env *env);
+JVHDEF jvh_error jvh_stop(struct jvh_env *env);
+
+JVHDEF jvh_error jvh_request(struct jvh_env *env, const char *server_name, const char *port, jvh_response *response, const char *requestTemplate, ...);
+
+JVHDEF jvh_error jvh_simple_get(struct jvh_env *env, const char *server_name, const char *port, const char *url, jvh_response *response) {
+    return jvh_request(env, server_name, port, response, "GET %s HTTP/1.0\r\n\r\n", url);
+}
+
+JVHDEF jvh_error jvh_recv_chunk(jvh_response *response, char *return_buffer, int buffer_size, int *bytes_read);
+JVHDEF jvh_error jvh_close(jvh_response *response);
+
+#define JV_HTTP_STATIC_ASSERT(COND, MSG) typedef char static_assertion_##MSG[(COND) ? 1 : -1]
+
+#ifdef JV_PLATFORM_WINSOCK
+// -----------------
+//  Windows structs
+// -----------------
+
+typedef struct jvh_env {
+    WSADATA wsa;
+} jvh_env;
+
+JV_HTTP_STATIC_ASSERT(sizeof(SOCKET) <= sizeof(((jvh_response *)0)->_internal), jvh_response_internal_field_must_be_big_enough_for_SOCKET);
+
+#else
+// ---------------------
+//  BSD Sockets structs
+// ---------------------
+
+#endif
+
+// ===========================================================================
+//  Example program:
+// ===========================================================================
+#ifdef JV_HTTP_TEST
+#define CHECK(x)                                              \
+    if((err = x) != JVH_ERR_OK) {                             \
+        printf("ERROR: code %d at line %d\n", err, __LINE__); \
+        exit(-1);                                             \
+    }
+
+#define RESPONSE_SIZE 2048
+
+int main(int argc, wchar_t *argv[]) {
+    jvh_error err;
+    jvh_env jvhEnvironment;
+    CHECK(jvh_init(&jvhEnvironment));
+    jvh_response Response;
+    CHECK(jvh_simple_get(&jvhEnvironment, "hombrealto.com", "80", "/", &Response));
+    char response[RESPONSE_SIZE + 1];
+    int BytesRead;
+    CHECK(jvh_recv_chunk(&Response, response, RESPONSE_SIZE, &BytesRead));
+    while(BytesRead != 0) {
+        response[BytesRead] = 0;
+        printf("%s", response);
+        CHECK(jvh_recv_chunk(&Response, response, RESPONSE_SIZE, &BytesRead));
+    }
+    CHECK(jvh_close(&Response));
+    CHECK(jvh_stop(&jvhEnvironment));
+
+    return 0;
+}
+
+#define JV_HTTP_IMPLEMENTATION
+
+#undef CHECK
+#endif
+
+#ifdef JV_HTTP_IMPLEMENTATION
+// ===========================================================================
+
+#ifdef JV_PLATFORM_WINSOCK
+// -----------------
+//  Windows implementation
+// -----------------
+
+jvh_error jvh__translate_wsaerror(int err) {
+    // clang-format off
+    switch(err) {
+        case 0:                     return JVH_ERR_OK;
+        case WSAEACCES:             return JVH_ERR_PERMISSION_DENIED;
+        case WSAEHOSTUNREACH:       return JVH_ERR_HOST_UNREACHABLE;
+        case WSAENETUNREACH:        return JVH_ERR_NET_UNREACHABLE;
+        case WSA_NOT_ENOUGH_MEMORY: return JVH_ERR_OUT_OF_MEMORY;
+        case WSAEAFNOSUPPORT:       // v
+        case WSAEOPNOTSUPP:         // v
+        case WSAEPROTONOSUPPORT:    // v
+        case WSAESOCKTNOSUPPORT:    // v
+        case WSAVERNOTSUPPORTED:    return JVH_ERR_UNSUPPORTED;
+        case WSAETIMEDOUT:          return JVH_ERR_TIMEOUT;
+        case WSAECONNABORTED:       return JVH_ERR_CONN_ABORTED;
+        case WSAECONNREFUSED:       return JVH_ERR_CONN_REFUSED;
+        case WSAENETRESET:          // v
+        case WSAECONNRESET:         return JVH_ERR_CONN_RESET;
+        case WSAEINVALIDPROCTABLE:  // v
+        case WSAEFAULT:             return JVH_ERR_BAD_MEM_ADDRESS;
+        case WSAEPROTOTYPE:         // v
+        case WSAEINVAL:             return JVH_ERR_INVALID_ARGUMENT;
+        case WSAENETDOWN:           return JVH_ERR_NET_DOWN;
+        case WSAEPROCLIM:           // v
+        case WSAEMFILE:             return JVH_ERR_TOO_MANY_CONNS;
+
+        // @TODO: Keep assigning jvh_error_codes to these errors
+        case WSAEADDRINUSE:
+        case WSAEADDRNOTAVAIL:
+        case WSAEALREADY:
+        case WSAEINPROGRESS:
+        case WSAEINTR:
+        case WSAEISCONN:
+        case WSAEMSGSIZE:
+        case WSAENOBUFS:
+        case WSAENOTCONN:
+        case WSAENOTSOCK:
+        case WSAEPROVIDERFAILEDINIT:
+        case WSAESHUTDOWN:
+        case WSAEWOULDBLOCK:
+        case WSAHOST_NOT_FOUND:
+        case WSANOTINITIALISED:
+        case WSANO_RECOVERY:
+        case WSASYSNOTREADY:
+        case WSATRY_AGAIN:
+        case WSATYPE_NOT_FOUND:
+
+        //
+        case WSAEINVALIDPROVIDER:   // v
+        default:                    return JVH_ERR_UNKNOWN;
+    }
+    // clang-format on
+}
+
+JVHDEF jvh_error jvh_init(jvh_env *env) {
+    int errcode = WSAStartup(MAKEWORD(2, 2), &env->wsa);
+    return jvh__translate_wsaerror(errcode);
+}
+
+JVHDEF jvh_error jvh_stop(jvh_env *env) {
+    int errcode = WSACleanup();
+    return jvh__translate_wsaerror(errcode);
+}
+
+#define RESP_SOCKET(resp) (*(SOCKET *)resp->_internal)
+
+static int jvh__connect(struct jvh_env *env, const char *server_name, const char *port, jvh_response *response) {
+    struct addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    struct addrinfo *server_addr;
+
+    // Resolve the server address and port
+    int addr_info_error = getaddrinfo(server_name, port, &hints, &server_addr);
+    if(addr_info_error != 0) {
+        return jvh__translate_wsaerror(addr_info_error);
+    }
+    memset(response, 0, sizeof(jvh_response));
+
+    RESP_SOCKET(response) = INVALID_SOCKET;
+
+    // Attempt to connect to an address until one succeeds
+    for(struct addrinfo *ptr = server_addr; ptr; ptr = ptr->ai_next) {
+        // Create a SOCKET for connecting to server
+        RESP_SOCKET(response) = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if(RESP_SOCKET(response) == INVALID_SOCKET) {
+            return jvh__translate_wsaerror(WSAGetLastError());
+        }
+
+        // Connect to server.
+        if(connect(RESP_SOCKET(response), ptr->ai_addr, (int)ptr->ai_addrlen) == SOCKET_ERROR) {
+            closesocket(RESP_SOCKET(response));
+            RESP_SOCKET(response) = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(server_addr);
+
+    if(RESP_SOCKET(response) == INVALID_SOCKET) {
+        return jvh__translate_wsaerror(WSAGetLastError());
+    }
+
+    return JVH_ERR_OK;
+}
+
+static jvh_error jvh__send(jvh_response *response, const char *data, int datalen) {
+    if(send(RESP_SOCKET(response), data, datalen, 0) == SOCKET_ERROR) {
+        jvh_error err = jvh__translate_wsaerror(WSAGetLastError());
+        closesocket(RESP_SOCKET(response));
+        return err;
+    }
+    return JVH_ERR_OK;
+}
+
+JVHDEF jvh_error jvh_recv_chunk(jvh_response *response, char *return_buffer, int buffer_size, int *bytes_read) {
+    // First check if we have something buffered in the response
+    if(response->_buffer_offset != 0 && response->_buffer_offset < response->_bytes_in_buffer) {
+        int left_in_buffer = response->_bytes_in_buffer - response->_buffer_offset;
+        int bytes_to_copy = left_in_buffer;
+        if(bytes_to_copy > buffer_size) {
+            bytes_to_copy = buffer_size;
+        }
+        memcpy(return_buffer, response->_buffer + response->_buffer_offset, bytes_to_copy);
+        response->_buffer_offset += bytes_to_copy;
+        *bytes_read = bytes_to_copy;
+        return JVH_ERR_OK;
+    }
+
+    // Ok, no buffered data, request more from server
+    int _bytes_read = recv(RESP_SOCKET(response), return_buffer, buffer_size, 0);
+    if(_bytes_read > 0) {
+        *bytes_read = _bytes_read;
+        return JVH_ERR_OK;
+    } else if(_bytes_read == 0) {
+        *bytes_read = 0;
+        return JVH_ERR_OK;
+    } else {
+        jvh_error err = jvh__translate_wsaerror(WSAGetLastError());
+        closesocket(RESP_SOCKET(response));
+        return err;
+    }
+}
+
+JVHDEF jvh_error jvh_close(jvh_response *response) {
+    if(closesocket(RESP_SOCKET(response)) == SOCKET_ERROR) {
+        return jvh__translate_wsaerror(WSAGetLastError());
+    }
+    return JVH_ERR_OK;
+}
+
+static int jvh__str_find_first(const char *s, char c, int max_len) {
+    int result = -1;
+    for(int i = 0; *s && i < max_len; s++, i++) {
+        if(*s == c) {
+            result = i;
+            break;
+        }
+    }
+    return result;
+}
+
+static jvh_error jvh__parse_headers(jvh_response *response) {
+    int bytes_read;
+    int errcode;
+    if((errcode = jvh_recv_chunk(response, response->_buffer, JV_HTTP_RESPONSE_BUFFER_LEN, &bytes_read)) != 0) {
+        return jvh__translate_wsaerror(errcode);
+    }
+    if(bytes_read == 0) {
+        return JVH_ERR_INVALID_RESPONSE; // server didn't return anything
+    }
+    // First line of response is of the form: HTTP/1.1 200 OK
+    // So we want to find the first space char
+    int status_code_pos = jvh__str_find_first(response->_buffer, ' ', bytes_read);
+    response->status_code = atoi(response->_buffer + status_code_pos);
+
+    // Ignore subsequent lines until we reach the \r\n\r\n
+    int cur_pos = 0;
+    int next_line_pos = jvh__str_find_first(response->_buffer, '\n', bytes_read);
+    while(next_line_pos - cur_pos > 2) {
+        cur_pos = next_line_pos + 1;
+        next_line_pos = cur_pos + jvh__str_find_first(response->_buffer + cur_pos, '\n', bytes_read - cur_pos);
+    }
+
+    response->_buffer_offset = next_line_pos + 1;
+    response->_bytes_in_buffer = bytes_read;
+    return JVH_ERR_OK;
+}
+
+JVHDEF jvh_error jvh_request(struct jvh_env *env, const char *server_name, const char *port, jvh_response *response, const char *request_template, ...) {
+    int errcode = 0;
+    jvh_error result = JVH_ERR_OK;
+    char *reqData = NULL;
+
+    if((errcode = jvh__connect(env, server_name, port, response)) != 0) {
+        goto handleErr;
+    }
+    va_list args;
+    va_start(args, request_template);
+
+    int reqDataLen = vsnprintf(NULL, 0, request_template, args);
+    reqData = (char *)JV_HTTP_MALLOC(reqDataLen + 1);
+    vsprintf(reqData, request_template, args);
+    reqData[reqDataLen] = 0;
+
+    if((errcode = jvh__send(response, reqData, reqDataLen)) != 0) {
+        goto handleErr;
+    }
+
+    if((errcode = jvh__parse_headers(response)) != 0) {
+        goto handleErr;
+    }
+
+    goto tearDown;
+
+handleErr:
+    result = jvh__translate_wsaerror(errcode);
+    jvh_close(response);
+
+tearDown:
+    JV_HTTP_FREE(reqData);
+    return result;
+}
+
+#else
+// ----------------------------
+//  BSD Sockets implementation
+// ----------------------------
+
+// @TODO: do all this!
+#endif // !JV_PLATFORM_WINSOCK
+
+#endif // JV_HTTP_IMPLEMENTATION
+#endif // JV_INCLUDE_HTTP_H
